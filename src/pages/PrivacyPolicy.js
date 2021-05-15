ad68a937e9a85b0b81613403a73b21442c10db8f
ssu00 <rachel4535@naver.com>
2021-05-15 18:15:59 +0900

    이용약관페이지

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Template from "../components/login/Template";
+import Privacy from "../components/login/Privacy";
+import Responsive from "../components/common/Responsive";
+
+const PrivacyPolicy = () => {
+  return (
+    <Responsive>
+      <Template>
+        <Privacy />
+      </Template>
+    </Responsive>
+  );
+};
+
+export default PrivacyPolicy;
