60416c4926a6b0d8bd1ed7b4eabeda27150639a0
ssu00 <rachel4535@naver.com>
2021-05-15 18:17:08 +0900

    아이디/비번 찾기-이메일인증 페이지

---
+++ src/components/login/FindAuth.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logoImg2 from "./logo2.png";
+import "./common.scss";
+import "./findauth.scss";
+
+const style = { display: "inline-block" };
+const FindAuth = ({ form, onChange, onSubmit }) => {
+  return (
+    <>
+      <img className="logo3" src={logoImg2} alt="" />
+      <h3 className="h302">이메일이 발송되었습니다.</h3>
+      <h5>
+        이메일이 도착하지 않았나요?{" "}
+        <Link to="/">
+          <h5 className="h508" style={style}>
+            재전송
+          </h5>
+        </Link>
+      </h5>
+      <form onSubmit={onSubmit}>
+        <div className="buttonsection">
+          <button className="button03 style01" type="submit" name="btn">
+            <Link to="/login">로그인</Link>
+          </button>
+          <button className="button03 style02" type="submit" name="btn">
+            <Link to="/">메인 페이지</Link>
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default FindAuth;
