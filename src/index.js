b38a0dc452ee9fed5ba4ad89ee407347a8c62c14
lsh9955 <cqe9933@gmail.com>
2021-05-22 12:56:29 +0900

    pull 취소

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-import Mainpage from '../src/pages/MainPage'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
-        < Mainpage />
-    </BrowserRouter>,
-    document.getElementById('root'),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
