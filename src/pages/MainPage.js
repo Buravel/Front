ffef9a4fc1e2e89de3a5e7394a101fe6bd0c6328
shin seong cheol <topsky2008@gmail.com>
2021-05-24 14:40:36 +0900

    git main merge

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
 import 'bootstrap/dist/css/bootstrap.css';
-import Contact from '../components/mainpage/Contact';
+// import Contact from '../components/mainpage/Contact';
 import Mainpage from '../components/mainpage/Mainpage';
-import SelectBox from '../components/mainpage/SelectBox';
-import Advertise from '../components/mainpage/Advertise'
-import Product from '../components/mainpage/Product'
-import Mainpage_afterlogin from '../components/mainpage/Mainpage_afterlogin'
-import After_topBar from '../components/mainpage/After_topBar'
-import After_Topnav from '../components/mainpage/After_Topnav'
-import Footer from '../components/mainpage/Footer';
-
+// import SelectBox from '../components/mainpage/SelectBox';
+// import Advertise from '../components/mainpage/Advertise';
+// import Product from '../components/mainpage/Product';
+// import Mainpage_afterlogin from '../components/mainpage/Mainpage_afterlogin';
+// import After_topBar from '../components/mainpage/After_topBar';
+// import After_Topnav from '../components/mainpage/After_Topnav';
+// import Mainpage from '../src/pages/MainPage';
+// import Footer from '../components/mainpage/Footer';
 
 const MainPage = () => {
     return (
         <div>
-        <Responsive>
-            <div>mainPage</div>
-        </Responsive>
+            <Responsive>
+                <Mainpage />
+            </Responsive>
 
-        <Contact/>
+            {/* <Contact /> */}
         </div>
     );
 };
